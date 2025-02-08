d103d1c82ca534a74143095b61daa5698c37cd83
CoolSpidey89 <fireraider89@gmail.com>
2025-02-08 11:15:00 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -36,16 +36,15 @@ async def start(update: Update, context: CallbackContext) -> None:
         caption = f"""
         ***Heyyyy...***
 
-***I am An Open Source Character Catcher Bot...​Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /guess to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
+***I am An Pokemon Card Collector Bot...​Add Me in Your group.. And I will send Random Pokemons After.. every Fixed Number of messages in Group... Use /guess to.. Collect that Pokemon in Your Collection.. and see Collection by using /collection... So add in Your groups and Gotta Catch em' all***
         """
         
         keyboard = [
-            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("ADD ME", url=f'http://t.me/PokeCollectBot?startgroup=new')],
+            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/Pokecollect'),
+            InlineKeyboardButton("UPDATES", url=f'https://t.me/pokecardcollect')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
-        ]
+          ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         photo_url = random.choice(PHOTO_URL)
 
@@ -54,11 +53,11 @@ async def start(update: Update, context: CallbackContext) -> None:
     else:
         photo_url = random.choice(PHOTO_URL)
         keyboard = [
-            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("ADD ME", url=f'http://t.me/PokeCollectBot?startgroup=new')],
+            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/Pokecollect'),
+            InlineKeyboardButton("UPDATES", url=f'https://t.me/pokecardcollect')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
+            
         ]
         
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -80,7 +79,7 @@ async def button(update: Update, context: CallbackContext) -> None:
 ***/topgroups : See Top Groups.. Ppl Guesses Most in that Groups***
 ***/top: Too See Top Users***
 ***/ctop : Your ChatTop***
-***/changetime: Change Character appear time (only works in Groups)***
+***/changetime: Change Pokemon appear time (only works in Groups)***
    """
         help_keyboard = [[InlineKeyboardButton("⤾ Bᴀᴄᴋ", callback_data='back')]]
         reply_markup = InlineKeyboardMarkup(help_keyboard)
@@ -92,16 +91,16 @@ async def button(update: Update, context: CallbackContext) -> None:
         caption = f"""
         ***Hoyyyy...*** ✨
 
-***I am An Open Source Character Catcher Bot..​Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /guess to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
+***I am A Pokemon Card Collect Bot..​Add Me in Your group.. And I will send Random Pokemons After.. Fixed Number of messages in Group... Use /guess to.. Collect that Pokemon in Your Pokedex.. and see Collection by using /collection... So add in Your groups and Gotta Catch em' all ***
         """
 
         
         keyboard = [
-            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("ADD ME", url=f'http://t.me/PokeCollectBot?startgroup=new')],
+            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/Pokecollect'),
+            InlineKeyboardButton("UPDATES", url=f'https://t.me/pokecardcollect')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
+        
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
